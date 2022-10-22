5e2d59e1b22fa641a9718a637b59c27e092f01f0
akulgiul <luka@4luig.de>
2022-10-22 23:53:21 +0200

    completed exercise 4.9

---
+++ chapter4/ex4_9.py
@@ -0,0 +1,21 @@
+# Exercise 4.9: Find Crossing Points of Two Graphs
+import numpy as np
+
+
+def f(x): return x
+def g(x): return x ** 2
+
+
+N = int(input("How many steps on interval [-4, 4]?\n"))
+epsilon = float(input("Give an epsilon:\n"))
+
+interval = np.linspace(-4, 4, N)
+crossing_points = []
+
+for x in interval:
+    difference = abs(f(x) - g(x))
+    if (difference < epsilon):
+        crossing_points.append(x)
+
+print("crossing points are:")
+print(crossing_points)
