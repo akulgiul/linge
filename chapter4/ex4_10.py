a6257b31c04f2d85e1b4371416536a8285bf0675
akulgiul <luka@4luig.de>
2022-11-08 21:59:07 +0100

    fixed program, now working solution to exercise

--- chapter4/ex4_10.py
+++ chapter4/ex4_10.py
@@ -7,37 +7,40 @@ import numpy as np
 
 def interpolate(measurements, delta, time):
     # check if time is in range of measurements
-    if time > measurements.size() * delta:
-        return -1
+    # if time > measurements.size() * delta:
+    #     return -1
 
     # finding i such that t_i <= time <= t_i+1
     i = 0
     while (i * delta < time):
         i += 1
+    print(f"i: {i}")
 
-    # getting y values of surrounding t_i and t_i+1
-    y1 = measurements[i]
-    y2 = measurements[i + 1]
+    # getting y values of surrounding t_i-1 and t_i
+    # and making line passing through (t_i-1, y1) and (t_i, y2)
+    x = [delta * (i - 1), delta * 1]
+    y = [measurements[i - 1], measurements[i]]
+    coefficients = np.polyfit(x, y, 1)
 
-    # calculating line passing through (t_i, y1) and (t_i+1, y2)
-    slope = y2 - y1
-    def f(x): return slope + (y1 - i * slope)
-
-    return f(time)
+    return coefficients[0] * time + coefficients[1]
 
 
 b = int(input("interval from 0 to ...?"))
 N = int(input("How many steps of measurements?"))
 time = float(input("Enter time for interpolation"))
 
+print(f"interval: [0, {b}]\nsteps: {N}\ntime: {time}")
+
 interval = np.linspace(0, b, N)
 delta = interval[1]
+
+print(f"interval from {interval[0]} to {interval[N - 1]}\ndelta: {delta}")
 
 
 def f(x): return x ** 2
 
 
-y_values = np.array([f(x) for x in interval])
+y_values = [f(x) for x in interval]
 
 y_time = interpolate(y_values, delta, time)
 print(y_time)
