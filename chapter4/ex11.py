3b88c22051e08f4039a9be679b9b0bba5a6e58a8
akulgiul <luka@4luig.de>
2022-11-09 19:05:49 +0100

    completed exercise 11

---
+++ chapter4/ex11.py
@@ -0,0 +1,22 @@
+# Exercise 4.11: Test Straight Line Requirement
+
+import numpy as np
+import random
+
+
+def f(x): return 4 * x + 1
+
+
+# draw 100 random values between 0 and 10
+random_x_values = [random.uniform(0, 10) for x in range(100)]
+print(f"drawn {len(random_x_values)} values\n")
+# print(random_x_values)
+
+c = 2
+calculated_slopes = []
+
+for x in random_x_values:
+    slope = (f(x) - f(c)) / (x - c)
+    calculated_slopes.append(slope)
+
+print(calculated_slopes)
